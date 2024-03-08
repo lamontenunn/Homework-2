93b704d6b8634f1d3abe759cacddcb4a80977d9e
LaMonte <lnunn6@emich.edu>
2024-03-08 18:42:08 -0500

    Pending changes to the sexMaritalClassArr to allow data to be properly converted

--- q1/Driver.java
+++ q1/Driver.java
@@ -1,12 +1,107 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Driver {
 
+    private static final int NEIGHBORS = 5;
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name of training data file: ");
+        String trainingDataIn = scanner.next();
+        System.out.print("Enter name of training output file name: ");
+        String trainingDataOut = scanner.next();
+
+        System.out.print("Enter name of classifed data file name: ");
+        String classifedFileName = scanner.next();
+
+       
+        normalizeTrainingData(trainingDataIn, trainingDataOut);
 
 
-    public static void main(String[] args) {
+
+
+        NearestNeighbor n = new NearestNeighbor();
+
+        n.loadTrainingData(trainingDataOut);
         
     }
+
+    private static void normalizeTrainingData(String input, String output) throws IOException {
+
+        Scanner in = new Scanner(new File(input));
+        PrintWriter out = new PrintWriter(new FileWriter(output));
+
+       
+
+        int numberOfRecords = in.nextInt();
+        int numberOfAttributes = in.nextInt();
+        int numberOfClasses = in.nextInt();
+
+        out.println(numberOfRecords + " " + numberOfAttributes + " " + numberOfClasses);
+
+        for (int i = 0; i < numberOfRecords; i++) {
+
+            int[] sexMaritalClassArr = new int[9];
+
+
+            // credit score
+            int creditScore = in.nextInt();
+            double creditNumber = normalizeData(creditScore, 500, 900);
+            out.print(creditNumber + " ");
+
+            // income
+            int income = in.nextInt();
+            double incomeNumber = normalizeData(income, 30, 90);
+            out.print(incomeNumber + " ");
+
+            // age
+            int age = in.nextInt();
+            double ageNumber = normalizeData(age, 30, 80);
+            out.print(ageNumber + " ");
+
+            // sex
+
+            String sex = in.next();
+            if(sex.equals("male")) sexMaritalClassArr[0] = 1; else sexMaritalClassArr[1] = 1;
+
+            // marital status
+            String maritalStatus = in.next();
+            if(maritalStatus.equals("single")) sexMaritalClassArr[2] = 1;
+            else if(maritalStatus.equals("married")) sexMaritalClassArr[3] = 1;
+            else if(maritalStatus.equals("divorced")) sexMaritalClassArr[4] = 1;
+
+   
+            // class
+            String riskClass = in.next();
+            if(riskClass.equals("low")) sexMaritalClassArr[5] = 1;
+            else if(riskClass.equals("medium")) sexMaritalClassArr[6] = 1;
+            else if(riskClass.equals("high")) sexMaritalClassArr[7] = 1;
+            else if(riskClass.equals("undetermined")) sexMaritalClassArr[8] = 1;
+
+            for(int num : sexMaritalClassArr) out.print(num+"");
+            out.println();
+
+
+        }
+        in.close();
+        out.close();
+
+    }
+
+    private static double normalizeData(double number, double min, double max) {
+
+        // this algorithm will convert the credit score to a double
+        // on the scale from 0 - 1 using Min-Max Normalization
+
+        return (number - min) / (max - min);
+
+    }
+
 
 
 
--- q1/NearestNeighbor.java
+++ q1/NearestNeighbor.java
@@ -6,6 +6,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NearestNeighbor {
+
+    private class Record 
+    {
+        private double[] attributes;         //attributes of record      
+        private int className;               //class of record
+
+        //Constructor of Record
+        private Record(double[] attributes, int className)
+        {
+            this.attributes = attributes;    //set attributes 
+            this.className = className;      //set class
+        }
+    }
     
 
     public NearestNeighbor() {
@@ -28,43 +41,186 @@ public class NearestNeighbor {
 
 
 
-    public void loadTrainingData(String trainingFile) throws IOException
-    {
-         Scanner inFile = new Scanner(new File(trainingFile));
-
-         //read number of records, attributes, classes
-         numberOfRecords = inFile.nextInt();
-         numberOfAttributes = inFile.nextInt();
-         numberOfClasses = inFile.nextInt();
-
-         //create empty list of records
-         records = new ArrayList<Record>();        
-
-         //for each record
-         for (int i = 0; i < numberOfRecords; i++)    
-         {
-             //create attribute array
-             double[] attributeArray = new double[numberOfAttributes]; 
-                                     
-             //read attribute values
-             for (int j = 0; j < numberOfAttributes; j++)   
-                  attributeArray[j] = inFile.nextDouble();  
- 
-             //read class name
-             int className = inFile.nextInt();
-
-             //create record
-
-// Record record = new Record(attributeArray, className);
-
-             //add record to list of records
-             
-// records.add(record);
-         }
-
-         inFile.close();
+    public void loadTrainingData(String trainingFile) throws IOException {
+        Scanner inFile = new Scanner(new File(trainingFile));
+    
+        // Read number of records, attributes, classes
+        numberOfRecords = inFile.nextInt();
+        numberOfAttributes = inFile.nextInt();
+        numberOfClasses = inFile.nextInt();
+    
+        // Create empty list of records
+        records = new ArrayList<Record>();
+    
+        // For each record
+        for (int i = 0; i < numberOfRecords; i++) {
+            // Read the entire attribute string for a record
+            String attributeString = inFile.next();
+    
+            // Placeholder for starting index of each attribute in the string
+            int startIdx = 0;
+    
+            // Create attribute array
+            double[] attributeArray = new double[numberOfAttributes];
+    
+            // Parse string into attributes
+            for (int j = 0; j < numberOfAttributes && startIdx < attributeString.length(); j++) {
+                int endIdx = startIdx + j + 2; // Calculate end index for the current attribute
+                if (endIdx > attributeString.length()) endIdx = attributeString.length(); // Adjust if beyond string length
+    
+                // Convert substring to double and assign to attribute array
+                attributeArray[j] = Double.parseDouble(attributeString.substring(startIdx, endIdx));
+    
+                // Update start index for next attribute
+                startIdx = endIdx;
+            }
+    
+            // Read class name (assuming it's still an integer at the end of each record's line)
+            int className = inFile.nextInt();
+    
+            // Create and add record to list of records
+            Record record = new Record(attributeArray, className);
+            records.add(record);
+        }
+    
+        inFile.close();
     }
 
+
+
+
+    void setParamaters(int num) {
+        numberOfNeighbors = num;
+
+
+    }
+    
+
+
+
+/*************************************************************************/
+
+    //Method determines the class of a set of attributes
+    private int classify(double[] attributes)
+    {
+        
+        double[] distance = new double[numberOfRecords];
+        int[] id = new int[numberOfRecords];
+
+
+        // need to convert the array in records first
+        for(int i =0;i<numberOfRecords;i++) {
+            distance[i] = distance(attributes,records.get(i).attributes);
+            id[i] = i;
+        }
+
+        nearestNeighbor(distance, id);
+
+        int className = majority(id);
+
+
+        return className;
+
+        
+
+
+
+    }
+
+    /*************************************************************************/
+
+    //Method finds the nearest neighbors
+    private void nearestNeighbor(double[] distance, int[] id)
+    {
+        //sort distances and choose nearest neighbors
+        for (int i = 0; i < numberOfNeighbors; i++)
+            for (int j = i; j < numberOfRecords; j++)
+                if (distance[i] > distance[j])
+                {
+                    double tempDistance = distance[i];
+                    distance[i] = distance[j];
+                    distance[j] = tempDistance;
+
+                    int tempId = id[i];
+                    id[i] = id[j];
+                    id[j] = tempId;
+                }
+    }
+
+    /*************************************************************************/
+
+    //Method finds the majority class of nearest neighbors
+    private int majority(int[] id)
+    {
+        double[] frequency = new double[numberOfClasses];
+
+        //class frequencies are zero initially
+        for (int i = 0; i < numberOfClasses; i++)
+            frequency[i] = 0;
+
+        //each neighbor contributes 1 to its class
+        for (int i = 0; i < numberOfNeighbors; i++)
+            frequency[records.get(id[i]).className - 1] += 1;
+
+        //find majority class
+        int maxIndex = 0;                         
+        for (int i = 0; i < numberOfClasses; i++)   
+            if (frequency[i] > frequency[maxIndex])
+               maxIndex = i;
+
+        return maxIndex + 1;
+    }
+
+    /*************************************************************************/
+
+
+    private double distance(double[] u, double[] v)
+    {
+        double distance = 0;         
+
+        for (int i = 0; i < u.length; i++)
+            distance = distance + (u[i] - v[i])*(u[i] - v[i]);
+
+        distance = Math.sqrt(distance); 
+ 
+        return distance;               
+    }
+    
+
+    private int findArrDistance(int[] arr1, int[] arr2) {
+
+        int arrDistance = 0;
+        
+        
+        for(int i =0;i<arr1.length;i++) if(arr1[i] != arr2[i]) arrDistance+= 1;
+
+
+
+
+
+        return arrDistance;
+
+    }
+
+
+
+    public void validate(String file) {
+
+
+        for(int i =0;i<numberOfRecords;i++) {
+
+
+            for(int j=i+1;j<numberOfRecords;j++) {
+
+                
+
+
+
+            }
+        }
+
+
+    }
 
 
 }
